092334b6d8fb523493bbc7b49865f7602f0949e8
Hudson Holanda <hudsonholanda55@gmail.com>
2024-08-02 20:47:08 -0300

    refact: change error reply

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify"
 import { z } from "zod"
 import { RegisterUseCase } from "@/use-cases/register"
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
+import { UserAlreadyExistsError } from "@/use-cases/errors/use-already-exists"
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -22,7 +23,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password
     })
   } catch (error) {
-    return reply.status(409).send()
+    if(error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({
+        message: error.message
+      })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
