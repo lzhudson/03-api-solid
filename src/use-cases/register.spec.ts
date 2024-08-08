9816cd3234376434aa8c1b85521124aecf9a02ce
Hudson Holanda <hudsonholanda55@gmail.com>
2024-08-08 08:18:54 -0300

    fix: use function before each inside in vitest package

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,9 +1,8 @@
-import { expect, test, describe, it } from "vitest";
+import { expect, beforeEach, describe, it } from "vitest";
 import { RegisterUseCase } from "./register";
 import { compare } from "bcryptjs";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { UserAlreadyExistsError } from "./errors/use-already-exists";
-import { beforeEach } from "node:test";
 
 let usersRepository: InMemoryUsersRepository;
 let registerUseCase: RegisterUseCase;
