e1f8d31c0f72e5a985c41b2f3d11b812fbe58888
Hudson Holanda <hudsonholanda55@gmail.com>
2024-08-15 21:28:06 -0300

    feat: implement validation hour in check in

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,7 @@
 import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/checkins-repository";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import dayjs from "dayjs";
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string
@@ -24,6 +25,15 @@ export class ValidateCheckInUseCase {
       throw new ResourceNotFoundError()
     }
 
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes'
+    );
+
+    if(distanceInMinutesFromCheckInCreation > 20) {
+      throw new Error()
+    }
+    
     checkIn.validated_at = new Date()
 
     await this.checkInsRepository.save(checkIn)
