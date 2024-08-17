88f22606d845576afd28cf7b6e54e346a6c142b0
Hudson Holanda <hudsonholanda55@gmail.com>
2024-08-17 11:27:20 -0300

    feat: implement jwt

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,33 +1,43 @@
-import { FastifyReply, FastifyRequest } from "fastify"
-import { z } from "zod"
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
-import { AuthenticateUseCase } from "@/use-cases/authenticate"
-import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error"
-import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case"
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
+import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 
-export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
   const authenticateBodySchema = z.object({
     email: z.string(),
-    password: z.string().min(6)
-  })
+    password: z.string().min(6),
+  });
 
-  const { email, password } = authenticateBodySchema.parse(request.body)
+  const { email, password } = authenticateBodySchema.parse(request.body);
 
   try {
     const registerUseCase = makeAuthenticateUseCase();
 
-    await registerUseCase.execute({
-      email, 
-      password
-    })
+    const { user } = await registerUseCase.execute({
+      email,
+      password,
+    });
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+    return reply.status(200).send({
+      token,
+    });
   } catch (error) {
-    if(error instanceof InvalidCredentialsError) {
+    if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({
-        message: error.message
-      })
+        message: error.message,
+      });
     }
-    throw error
+    throw error;
   }
-
-  return reply.status(200).send()
 }
