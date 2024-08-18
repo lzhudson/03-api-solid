7286004c420470c27cb24d5d6e240b6bb866ebc0
Hudson Holanda <hudsonholanda55@gmail.com>
2024-08-18 11:30:14 -0300

    refact: return status 200

--- src/http/controllers/check-ins/create.ts
+++ src/http/controllers/check-ins/create.ts
@@ -29,5 +29,5 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     userLongitude: longitude
   });
 
-  return reply.status(201).send();
+  return reply.status(200).send();
 }
