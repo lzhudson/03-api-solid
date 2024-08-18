e7c9c67f9ee6ba09bb43a7a4fe80e6cb146fd93f
Hudson Holanda <hudsonholanda55@gmail.com>
2024-08-18 11:06:09 -0300

    feat: create test to list all gyms by title

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,57 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/creat-and-authenticate-user";
+
+describe("Search Gyms (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be search gyms by title", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+    
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "JavaScript Gym",
+        description: "Some description",
+        phone: "1119199191",
+        latitude: -3.7912576,
+        longitude: -38.551552,
+      });
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "TypeScript Gym",
+        description: "Some description",
+        phone: "1119199191",
+        latitude: -3.7912576,
+        longitude: -38.551552,
+      });
+      
+    const response = await request(app.server)
+      .get("/gyms/search")
+      .query({
+        q: "JavaScript",
+      })
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+
+    expect(response.statusCode).toEqual(201);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'JavaScript Gym'
+      })
+    ]);
+  });
+});
