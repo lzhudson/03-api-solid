f5e359ab4591d84a80bc27c7bb195663ed7873d5
Hudson Holanda <hudsonholanda55@gmail.com>
2024-08-18 11:05:19 -0300

    feat: create test for create gym

---
+++ src/http/controllers/gyms/creeate.spec.ts
@@ -0,0 +1,31 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { createAndAuthenticateUser } from "@/utils/test/creat-and-authenticate-user";
+
+describe("Create Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be to create a gym", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+    
+    const response = await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '1119199191',
+        latitude: -3.7912576,
+        longitude: -38.551552,
+      })
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
