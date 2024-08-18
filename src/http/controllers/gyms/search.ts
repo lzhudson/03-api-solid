052e15109b8bd7702182399f2e0765873a144402
Hudson Holanda <hudsonholanda55@gmail.com>
2024-08-18 11:06:20 -0300

    refact: add page with min and default value

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -5,7 +5,7 @@ import { makeSearchGymsUseCase } from "@/use-cases/factories/make-search-gyms-us
 export async function search(request: FastifyRequest, reply: FastifyReply) {
   const searchGymsQuerySchema = z.object({
     q: z.string(),
-    page: z.coerce.number()
+    page: z.coerce.number().min(1).default(1)
   });
 
   const { q, page } =
