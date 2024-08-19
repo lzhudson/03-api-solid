3c57cffcf5d87da202d8f8c4d36e491a9542e17c
Hudson Holanda <hudsonholanda55@gmail.com>
2024-08-19 09:14:54 -0300

    feat: implement refresh token

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,24 @@ import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { userRoutes } from "./http/controllers/users/routes";
 import { gymRoutes } from "./http/controllers/gyms/routes";
 import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 
 export const app = fastify();
 app.register(fastifyJwt, {
-  secret: env.JWT_SECRET
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '10m'
+  }
 })
+app.register(fastifyCookie);
+
 app.register(userRoutes);
 app.register(gymRoutes);
 app.register(checkInsRoutes);
